6548f8f073b9e4641bae5670672a66ad6c4a10a0
luka177 <lukapanio@gmail.com>
2020-06-04 18:59:24 +0300

    Fix typo

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
 lunch omni_$DEVICE-userdebug
-nkdir -p /home/runner/work/orangefox/out/target/product/yggdrasil/system/etc
+mkdir -p /home/runner/work/orangefox/out/target/product/yggdrasil/system/etc
 touch /home/runner/work/orangefox/out/target/product/yggdrasil/system/etc/ld.config.txt
 make -j$(nproc) recoveryimage
 
