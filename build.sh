1b102595f46a02cc3249645a19b8c07e13cffab3
luka177 <lukapanio@gmail.com>
2020-07-24 12:42:49 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source of.env
 cd $ANDROID_ROOT
-
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export LC_ALL="C"
@@ -9,4 +8,3 @@ lunch omni_$DEVICE-userdebug
 mkdir -p /home/runner/work/orangefox/out/target/product/yggdrasil/system/etc
 touch /home/runner/work/orangefox/out/target/product/yggdrasil/system/etc/ld.config.txt
 make -j$(nproc) recoveryimage
-
